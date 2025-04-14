c262fe5da5e99e43179bbd8a55361321c03bb127
Tabbywambui331 <nungaitabby@gmail.com>
2025-04-14 10:17:18 +0300

    Your commit message here

--- 📄 Roles.sql
+++ 📄 Roles.sql
@@ -4,9 +4,9 @@ CREATE USER 'salesperson_user'@'localhost' IDENTIFIED BY 'Pass456!';
 CREATE USER 'Frontdesk_user'@'localhost' IDENTIFIED BY 'pass789!';
 
 -- Grant roles
-GRANT ALL PRIVILEGES ON BookstoreDB.* TO 'Bookstoreadmin_user'@'localhost';
+GRANT ALL PRIVILEGES ON BookstoreDB.* TO 'Bookstore_admin_user'@'localhost';
 GRANT SELECT, INSERT, UPDATE ON BookstoreDB.* TO 'salesperson_user'@'localhost';
-GRANT SELECT ON BookstoreDB.* TO 'Frontdesl_user'@'localhost';
+GRANT SELECT ON BookstoreDB.* TO 'Frontdesk_user'@'localhost';
 
 
 
